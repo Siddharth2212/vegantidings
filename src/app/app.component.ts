f0afb21aa7a10a04f16b6d55879e231c0df28b3b
Siddharth <siddharthsogani22@gmail.com>
2017-08-16 20:41:50 +0530

    Getting started

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,25 +4,29 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
-import { ListPage } from '../pages/list/list';
+import { AboutPage } from '../pages/about/about';
+import { MenuPage } from '../pages/menu/menu';
+import { ContactPage } from '../pages/contact/contact';
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  @ViewChild(Nav) nav: Nav;
+  @ViewChild(Nav) nav: Nav; //Sets up the navigation support Ionic has built into it's framework
 
   rootPage: any = HomePage;
 
-  pages: Array<{title: string, component: any}>;
+  pages: Array<{title: string, icon: string, component: any}>;
 
   constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
     this.initializeApp();
 
     // used for an example of ngFor and navigation
     this.pages = [
-      { title: 'Home', component: HomePage },
-      { title: 'List', component: ListPage }
+      { title: 'Home', icon: 'home', component: HomePage },
+      { title: 'About Us', icon: 'information-circle', component: AboutPage },
+      { title: 'Menu', icon: 'list-box', component: MenuPage },
+      { title: 'Contact Us', icon: 'contact', component: ContactPage }
     ];
 
   }
@@ -39,6 +43,6 @@ export class MyApp {
   openPage(page) {
     // Reset the content nav to have just this page
     // we wouldn't want the back button to show in this scenario
-    this.nav.setRoot(page.component);
+    this.nav.setRoot(page.component); //Each of the four pages act as root page of the navigation stack
   }
 }
